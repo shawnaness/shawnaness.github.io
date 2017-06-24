6caae8b2b654eac06b3984c5aac4f1815c4ef2d3
shawnaness <shawna_huang@brown.edu>
2017-06-23 21:09:12 -0400

    changes to navigation bar

---
+++ script.js
@@ -0,0 +1,25 @@
+const indexArray = document.querySelectorAll("li a");
+
+function changeIndexToHome() {
+  for (i = 0; i < indexArray.length; i++) {
+    indexArray[i].style.color = "#008080";
+  }
+  document.querySelector("li a.header").style.color = "black";
+  document.querySelector("li a.header").scrollIntoView(true);
+}
+
+function changeIndexToProjects() {
+  for (i = 0; i < indexArray.length; i++) {
+    indexArray[i].style.color = "#008080";
+  }
+  document.querySelector("li a.projects").style.color = "black";
+  document.querySelector("li a.projects").scrollIntoView(true);
+}
+
+function changeIndexToContacts() {
+  for (i = 0; i < indexArray.length; i++) {
+    indexArray[i].style.color = "#008080";
+  }
+  document.querySelector("li a.contact").style.color = "black";
+  document.querySelector("li a.contact").scrollIntoView(true);
+}
